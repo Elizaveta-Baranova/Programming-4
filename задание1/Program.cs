a9cf4cbd9676d3a36304e2fb569df36bceddd30d
Liza <elizaveta.b.elizaveta@yandex.by>
2022-09-04 22:12:53 +0300

    задание 1

---
+++ задание1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int number = ReadInt("Введите число: ");
+int degree = ReadInt("Введите степень числа: ");
+int i = 1;
+int result = number;
+while (i < degree)
+{
+    result = result * number;
+    i ++;
+}
+Console.Write($"{number} в степени {degree} = {result}.");
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
